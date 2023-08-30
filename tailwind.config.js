0754ec0456b47a07f30be3d8982f27e2f4854c8b
Ranu Putra Dewantara <ranuputradewantara@vk.com>
2023-08-30 16:11:18 +0700

    Fixing Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,7 @@ module.exports = {
         'font3': 'Coda',
         'font4': 'DAGGERSQUARE',
         'font5': 'Goldman',
-        'font6': 'Prosto One',
-        'font7': 'Play',
-        'font8': 'Revalia',
-        'font9': 'Iceberg',
+        'font6': 'Play',
       }
     },
   },
